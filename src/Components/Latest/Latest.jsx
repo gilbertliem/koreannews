3f3bb3d75174647c63525a6389df2428cc790939
gilbert yohanes <yohanesbert97@gmail.com>
2021-01-02 17:39:44 +0700

    add latest component

---
+++ src/Components/Latest/Latest.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Latest.module.css";
+
+// -------------------- //
+
+function Latest() {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+}
+
+export default Latest;
