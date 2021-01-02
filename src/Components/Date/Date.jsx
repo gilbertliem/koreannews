1af17e0afb6652389029ca43532c5387664215dc
gilbert yohanes <yohanesbert97@gmail.com>
2021-01-02 17:25:29 +0700

    add date component

---
+++ src/Components/Date/Date.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+// import styles from "Date.module.css";
+
+// -------------------- //
+
+function Date() {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+}
+
+export default Date;
