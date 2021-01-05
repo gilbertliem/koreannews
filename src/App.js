fc2bc8f0e40bfb7b2b43ea4794277f50ef5604a6
gilbert yohanes <yohanesbert97@gmail.com>
2021-01-05 10:38:41 +0700

    edit cover

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
+import Cover from "./Components/Cover/Cover";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Cover />
     </>
   );
 }
--- src/Components/Cover/Cover.jsx
+++ src/Components/Cover/Cover.jsx
@@ -1,12 +1,23 @@
 import React from "react";
-// import styles from './Cover.module.css'
+import styles from "./Cover.module.scss";
+import cover from "../../Assets/Images/social.jpg";
+// import Navbar from "../Navbar/Navbar";
 
 // -------------------- //
 
 function Cover() {
   return (
     <>
-      <div></div>
+      <div className={styles.cover}>
+        <img src={cover} alt="cover" className={styles.image} />
+        <div className={styles.container}>
+          {/* <Navbar /> */}
+          <div className={styles.info}>
+            <div className={styles.title}>Kpop News</div>
+            <div className={styles.subtitle}>subtitle</div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
