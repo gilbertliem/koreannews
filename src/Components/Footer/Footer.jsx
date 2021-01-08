2e88429103d085e31b7da7fe45d5aa9f4f5e4a7d
gilbert yohanes <yohanesbert97@gmail.com>
2021-01-08 20:51:16 +0700

    edit footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,12 +1,24 @@
 import React from "react";
-import styles from "./Footer.module.css";
+import styles from "./Footer.module.scss";
 
 // -------------------- //
 
 function Footer() {
   return (
     <>
-      <div></div>
+      <div className={styles.footer}>
+        <div className={styles.row}>
+          <div>About Us</div>
+          <div>Terms of Use</div>
+          <div>Privacy Policy</div>
+          <div>Copyright Policy</div>
+          <div>Advertisement</div>
+          <div>Newsletter</div>
+        </div>
+        <div className={styles.copyright}>
+          Copyright © 2020. All Rights Reserved.
+        </div>
+      </div>
     </>
   );
 }
