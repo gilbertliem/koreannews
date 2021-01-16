49aedc5ae54c5d620f4102797e06c5c06deebfc6
gilbert yohanes <yohanesbert97@gmail.com>
2021-01-16 14:55:30 +0700

    edit cover

--- src/Components/Cover/Cover.jsx
+++ src/Components/Cover/Cover.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import styles from "./Cover.module.scss";
-import cover from "../../Assets/Images/social.jpg";
+import cover from "../../Assets/Images/night.jpg";
+import cover1 from "../../Assets/Images/social.jpg";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper.scss";
 // import Navbar from "../Navbar/Navbar";
 
 // -------------------- //
@@ -8,16 +11,45 @@ import cover from "../../Assets/Images/social.jpg";
 function Cover() {
   return (
     <>
-      <div className={styles.cover}>
+      <Swiper
+        effect="fade"
+        spaceBetween={0}
+        slidesPerView={1}
+        autoplay={{ delay: 3000, disableOnInteraction: false }}
+      >
+        <SwiperSlide className={styles.cover}>
+          <div>
+            <img src={cover} alt="cover" className={styles.image} />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={styles.cover}>
+            <img src={cover1} alt="cover" className={styles.image} />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={styles.cover}>
+            <img src={cover} alt="cover" className={styles.image} />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={styles.cover}>
+            <img src={cover1} alt="cover" className={styles.image} />
+          </div>
+        </SwiperSlide>
+        ...
+      </Swiper>
+
+      {/* <div className={styles.cover}>
         <img src={cover} alt="cover" className={styles.image} />
         <div className={styles.container}>
-          {/* <Navbar /> */}
+          <Navbar />
           <div className={styles.info}>
             <div className={styles.title}>Kpop News</div>
             <div className={styles.subtitle}>subtitle</div>
           </div>
         </div>
-      </div>
+      </div> */}
     </>
   );
 }
